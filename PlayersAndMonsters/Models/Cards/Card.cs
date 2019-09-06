012ca34dd3b8ee8555250e40c7fcd28504ffebb6
Angel Hristov <angelxristov84@abv.bg>
2019-09-06 22:45:02 +0300

    Card imp.

---
+++ PlayersAndMonsters/Models/Cards/Card.cs
@@ -0,0 +1,13 @@
+﻿using PlayersAndMonsters.Models.Cards.Contracts;
+
+namespace PlayersAndMonsters.Models.Cards
+{
+    public class Card : ICard
+    {
+        public string Name { get; }
+
+        public int DamagePoints { get; set; }
+
+        public int HealthPoints { get; }
+    }
+}
